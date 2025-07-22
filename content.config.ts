94d73a0241f628d309238f5d70000aa85c8c43c2
Ari Gestetner <ariges770@gmail.com>
2025-07-22 23:38:47 +1000

    Added collections to content.config.ts

--- content.config.ts
+++ content.config.ts
@@ -15,18 +15,22 @@ export default defineContentConfig({
           github: z.string().url().optional(),
           linkedin: z.string().url().optional(),
         }).optional()
+      })
+    }),
+    mycards: defineCollection({
+      type: 'data',
+      source: 'mycards/*.json',
+      schema: z.object({
+        title: z.string(),
+        desc: z.string(),
+        url: z.string().url(),
+        src: z.string().url(),
+      })
+    }),
+    content: defineCollection({
+      type: 'page',
+      source: '**/*.md'
     })
-  }),
-  mycards: defineCollection({
-    type: 'data',
-    source: 'mycards/*.json',
-    schema: z.object({
-      title: z.string(), 
-      desc: z.string(),
-      url: z.string().url(), 
-      src: z.string().url(),
-    })
-  })
-
-}})
+  }
+})
 

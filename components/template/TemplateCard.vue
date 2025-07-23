d3c6581e3a1e253b78ce0e02a7a86356e666fe37
Ari Gestetner <ariges770@gmail.com>
2025-07-23 14:13:24 +1000

    Nearly completed cards (with some tiny adjustments required)

--- components/template/TemplateCard.vue
+++ components/template/TemplateCard.vue
@@ -1,18 +1,18 @@
 <template>
   <article class="col-6 col-12-xsmall work-item ds-card">
-    <div class="ds-card-inner image fit">
-        <div class="ds-card-front">
-      <a :href="item.url" class="">
-      </a>
-          <img :src="item.src" alt="" />
-        </div>
-        <!-- <img src="~/assets/img/previews/water-tap.jpg" alt=""/> -->
-
-        <div class="ds-card-back">
-          <h3 class="">{{ item.title }}</h3>
+    <a :href="item.url" class="ds-card-inner image fit">
+      <div class="ds-card-front">
+        <h3 class="ds-card-front-header">{{ item.title }}</h3>
+        <img :src="item.src" alt="" />
+      </div>
+      <div class="ds-card-back">
+        <div class="ds-card-back-bg" :style="{ backgroundImage: `url(${item.src})` }"></div>
+        <div class="ds-card-back-content">
+          <h3>{{ item.title }}</h3>
           <p>{{ item.desc }}</p>
         </div>
-    </div>
+      </div>
+    </a>
   </article>
 </template>
 
@@ -23,14 +23,9 @@ const props = defineProps({
     required: true,
   },
 });
-
-const defaultImage = "@/assets/img/img-01.jpg";
-const filePath = "/img/img-01.jpg";
-// item.image ? item.image.src :
 </script>
 
 <style>
-/* Double sided card inspo from https://prismic.io/blog/css-hover-effects#6-doublesided-card */
 .ds-card {
   width: 200px;
   height: 250px;
@@ -51,22 +46,14 @@ const filePath = "/img/img-01.jpg";
   transform: rotateY(180deg);
 }
 
-.ds-card-front img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* This ensures the image covers the entire container */
-}
-
 .ds-card-front,
 .ds-card-back {
   position: absolute;
   width: 100%;
   height: 100%;
   backface-visibility: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   border-radius: 0.5rem;
+  overflow: hidden;
 }
 
 .ds-card-front {
@@ -74,9 +61,64 @@ const filePath = "/img/img-01.jpg";
   border: 1px solid #ffffffff;
 }
 
+.ds-card-front img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.ds-card-front-header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 0.5rem;
+  background: rgba(0, 0, 0, 0.7);
+  color: white;
+  text-align: center;
+  z-index: 1;
+  margin: 0;
+}
+
 .ds-card-back {
-  background: grey;
-  color: black;
+  background: rgba(8, 21, 22, 0.85);
+  color: white;
   transform: rotateY(180deg);
+  position: relative;
+}
+
+.ds-card-back-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  filter: blur(8px);
+  opacity: 0.6;
+  transform: scaleX(-1);
+  z-index: 0;
+}
+
+.ds-card-back-content {
+  position: relative;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 1rem;
+  text-align: center;
+}
+
+.ds-card-back h3,
+.ds-card-back p {
+  text-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  margin: 0.5rem 0;
 }
 </style>

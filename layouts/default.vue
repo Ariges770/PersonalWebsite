8fb919c3c4bcfa23d489afdcbb5a8b8e8ae0dfc9
Ari Gestetner <ariges770@gmail.com>
2025-08-13 11:29:08 +1000

    General layout for website.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,10 @@
 <template>
-  <div>
+  <div class="default-layout">
     <TemplateHeader :headerpaths="headerpaths"/>
     <!-- Main -->
     <div id="main">
       <slot />
-
-      <!-- Footer -->
-      <footer id="footer" style="background-color: black">
-        <h1>Test</h1>
-        <div class="inner">
-          <ul class="icons">
-            <li>
-              <a href="#" class="icon brands fa-twitter"
-                ><span class="label">Twitter</span></a
-              >
-            </li>
-            <li>
-              <a href="#" class="icon brands fa-github"
-                ><span class="label">Github</span></a
-              >
-            </li>
-            <li>
-              <a href="#" class="icon brands fa-dribbble"
-                ><span class="label">Dribbble</span></a
-              >
-            </li>
-            <li>
-              <a href="#" class="icon solid fa-envelope"
-                ><span class="label">Email</span></a
-              >
-            </li>
-          </ul>
-          <ul class="copyright">
-            <li>&copy; Untitled</li>
-            <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-          </ul>
-        </div>
-      </footer>
+      <!-- <TemplateFooter /> -->
     </div>
   </div>
 
@@ -47,7 +15,7 @@
 const headerpaths = [
   { path: "/", title: "Home" },
   { path: "/blog", title: "Blog" },
-  { path: "/research", title: "Research" },
+  // { path: "/research", title: "Research" },
   { path: "/resume", title: "Resume" },
   { path: "/contact", title: "Contact" },
   { path: "/template", title: "Template" },
@@ -55,3 +23,9 @@ const headerpaths = [
 </script>
 
 
+<style>
+::selection {
+  background-color: #9effc6ff;
+  color: #09130dff;
+}
+</style>
